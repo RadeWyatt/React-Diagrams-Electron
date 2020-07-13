9f8cce8c8cd77fe635648faa6af46d6678e708f3
Raden <rade.pilkinton@wsu.edu>
2020-07-12 20:45:11 -0700

    scalable background:

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -12,13 +12,13 @@ export default class Canvas extends React.Component {
 
         }
         this.engine = createEngine();
-        this.model = new DiagramModel();
+        this.model = this.generateNewModel();
 
         let node1 = new DefaultNodeModel();
-        node1.setPosition(100, 100);
+        node1.setPosition(0, 0);
         let port1 = node1.addOutPort('Out');
         let node2 = new DefaultNodeModel();
-        node2.setPosition(200,200);
+        node2.setPosition(250,250);
         let port2 = node2.addOutPort('Out');
 
         let link = port1.link(port2);
@@ -26,9 +26,34 @@ export default class Canvas extends React.Component {
         this.engine.setModel(this.model);
     }
 
+    generateNewModel = () => {
+        let newModel = new DiagramModel();
+        newModel.setGridSize(250);
+        newModel.registerListener({
+            offsetUpdated: (e) => this.handleOffsetUpdate(e),
+            zoomUpdated: (e) => this.handleZoomUpdate(e),
+
+        })
+        return newModel;
+    }
+
+    handleOffsetUpdate = () => {
+        let offsetX = this.model.getOffsetX();
+        let offsetY = this.model.getOffsetY();
+        let container = document.getElementById("dcontainer");
+        container.style.setProperty('--offset-x', `${offsetX}px`)
+        container.style.setProperty('--offset-y', `${offsetY}px`);
+    }
+
+    handleZoomUpdate = (e) => {
+
+    }
+
     render = () => {
         return (
-            <CanvasWidget className="diagram-container" engine={this.engine}/>
+            <div id="dcontainer">
+                <CanvasWidget className="diagram-container" engine={this.engine}/>
+            </div>
         )
     }
 
